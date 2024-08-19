329c2050230991898fe32b23491069b87af9a1d4
unknown <alfinohasan629@gmail.com>
2024-08-19 21:49:28 +0700

    Fisrt commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+// use Inertia\Inertia; Halaman Inertia berhubungan dengan komponen Vue atau React
+// use Inertia\Response;
+
+class UserController extends Controller
+{
+
+}
